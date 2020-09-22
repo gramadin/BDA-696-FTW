ff8eb1a9a6c82c5e0250bc5c86857dacf7d35ae5
gramadin <33378682+gramadin@users.noreply.github.com>
2020-09-22 12:22:33 -0700

    Updates with more efficient code
    Rolling still missing.

--- HW2_esmythe_SQL.sql
+++ HW2_esmythe_SQL.sql
@@ -1,6 +1,7 @@
 /*
   Created by Ed Smythe Sept 2020
   BDA 696 @ SDSU
+  Current run time is ~ 12.605s
 */
 
 -- make unique batter list
@@ -8,80 +9,68 @@ drop table if exists player_bat_avg;
 create table player_bat_avg as
 select unique batter from atbat_r order by batter;
 
+alter table player_bat_avg add primary key (batter);
+
 -- make table to pull performance data from
 drop table if exists player_bat_avg_all;
 create table player_bat_avg_all as
-select batter_counts.batter, hit, game.game_id,  game.local_date from batter_counts 
+select batter_counts.game_id , batter , plateApperance , toBase, left(game.local_date,10) as Game_Date, (toBase/nullif(plateApperance,0)) as game_bat_avg
+from batter_counts 
 join game on batter_counts.game_id = game.game_id  
 order by batter, game_id;
 
+alter table player_bat_avg_all add primary key (batter, game_id);
+
 -- Make Report Table
 drop table if exists REPORT;
-create table REPORT as select distinct batter from player_bat_avg_all where batter = '400284';
+create table REPORT as select batter from player_bat_avg;
 alter table REPORT 
-  add historic float
+  add Historic float
     after batter,
-  add annual float 
-    after historic,
-  add rolling float
-   after annual;
-   
--- get specific batter performance
+  add _2007_ float 
+    after Historic,
+  add _2008_ float 
+    after _2007_,
+  add _2009_ float 
+    after _2008_,
+  add _2010_ float 
+    after _2009_,
+  add _2011_ float 
+    after _2010_,
+  add _2012_ float 
+    after _2011_;
+
+alter table REPORT add primary key (batter);
+  
+-- Make Histroic avg table and update REPORT
 drop table if exists hist_calc;
 create table hist_calc as
-select batter, count(case when hit = 0 then 1 end) as hit, count(hit) as bat
-from player_bat_avg_all
-where batter = '400284';-- chg
-/*this will turn into a user entered value in future iteration*/
+select batter, avg(game_bat_avg) as hist_avg from game_player  group by batter;
 
--- Make report table
-alter table hist_calc 
-  add hist_avg float;
-    
-update hist_calc set hist_avg = (hit/bat) where batter = '400284'; -- chg
-/*this will turn into a user entered value in future iteration*/
-
--- send results to report table
 update REPORT , hist_calc
 set REPORT.historic = hist_calc.hist_avg
 where REPORT.batter = hist_calc.batter;
 
--- Make annual table
+-- Make Annual Avg table and update REPORT
 drop table if exists annual_calc;
 create table annual_calc as
-select batter, count(case when hit = 0 then 1 end) as hit_an , count(hit) as bat_an
-from player_bat_avg_all
-where batter = '400284' and local_date like'2011%';-- chg
-/* these will turn into a user entered values in future iteration
-   select max(local_date), min(local_date) from game
-   Min = 2007-02-26 13:05:00 and max = 2012-06-28 22:15:00	*/
+select batter, left(player_bat_avg_all.Game_Date,4) as Game_Yr, avg(game_bat_avg) as ann_avg from player_bat_avg_all group by batter, Game_Yr;
 
-alter table annual_calc 
-  add annual float
-    after bat_an;
-
-update annual_calc set annual_calc.annual = (hit_an/bat_an);
-
--- send results to report table
-update REPORT , annual_calc
-set REPORT.annual = annual_calc.annual
-where REPORT.batter = annual_calc.batter;
-
--- Make rolling table
-drop table if exists rolling_calc;
-create table rolling_calc as
-select batter, count(case when hit = 0 then 1 end) as hit_rol , count(hit) as bat_rol
-from player_bat_avg_all
-where batter = '400284' and local_date >= (('2012-01-01')- interval 100 day);/* chg */ /*these will turn into a user entered values in future iteration*/
-
-alter table rolling_calc 
-  add rolling float
-    after bat_rol;
-
-update rolling_calc set rolling_calc.rolling = (hit_rol/bat_rol);
-
--- send results to report table
-update REPORT , rolling_calc
-set REPORT.rolling = rolling_calc.rolling
-where REPORT.batter = rolling_calc.batter;
-select * from report ;
+update REPORT, annual_calc 
+set report._2007_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2007';
+update REPORT, annual_calc 
+set report._2008_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2008';
+update REPORT, annual_calc 
+set report._2009_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2009';
+update REPORT, annual_calc 
+set report._2010_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2010';
+update REPORT, annual_calc 
+set report._2011_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2011';
+update REPORT, annual_calc 
+set report._2012_ = annual_calc.ann_avg
+where REPORT.batter = annual_calc.batter and annual_calc.Game_Yr = '2012';
