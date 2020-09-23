b590f209a71221d7de42a07f3f9fad370afc5a61
gramadin <33378682+gramadin@users.noreply.github.com>
2020-09-23 01:18:15 -0700

    updated names and adjusted join
    1 month of data completed @ 56 seconds. Full data process time unk.

--- rolling_attempt_esmythe.sql
+++ rolling_attempt_esmythe.sql
@@ -1,10 +1,11 @@
 /*
   Created by Ed Smythe Sept 2020
   BDA 696 @ SDSU
-  Current run time is ~ ?
+  Current run time is ~ ? one month is 56 seconds
   New Tables Created:
   --------------------------------------------
-  self_join_test	- Join player game average data on each other
+  prejoin	- Join player game average data on each other
+  final_t	- Rolling table
   
   Currently runs longer than 30 min and no reults, but no errors.
 */
@@ -12,14 +13,18 @@
 /* Comment lines below are from professor feed back*/
 
 -- get a list of all the games and players in those games.
-drop table if exists self_join_test;
-create table self_join_test as
-select distinct batter, game_id, Game_Date, game_bat_avg, date_sub(Game_Date, interval 100 DAY) as look_window from player_bat_avg_all
-order by batter ;
+drop table if exists prejoin;
+create table prejoin as
+	select batter, game_id, Game_Date, game_bat_avg, date_sub(Game_Date, interval 100 DAY) as look_window 
+	from player_bat_avg_all 
+	order by batter;
 -- for each of those rows, (playerid, game_id) join the table to itself 
 -- then put a restriction on that join on the dates (you may have to rope in a date table).
-select self_join_test.Game_Date, self_join_test.game_bat_avg, avg(rolling_self_join_test.game_bat_avg)
-from self_join_test
-join self_join_test as rolling_self_join_test
-	on rolling_self_join_test.Game_Date between self_join_test.Game_Date - 99 and self_join_test.Game_Date
-group by batter, game_id
+drop table if exists final_t;
+create table final_t as
+	select prejoin.batter, prejoin.game_id, prejoin.Game_Date, prejoin.game_bat_avg, avg(rolling.game_bat_avg) as rolling_avg
+	from prejoin
+	join prejoin as rolling
+		on prejoin.batter and rolling.Game_Date between prejoin.look_window and prejoin.Game_Date
+	group by prejoin.batter, game_id;
+	
