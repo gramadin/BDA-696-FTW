0acd005294d61c308b3a3d35b5c9142dd518ef84
gramadin <33378682+gramadin@users.noreply.github.com>
2020-09-29 14:49:28 -0700

    Update Spark_HW3.py

--- Spark_HW3.py
+++ Spark_HW3.py
@@ -16,30 +16,56 @@
   Do all the logic in Spark
 """
 
+import sys
+import tempfile
+
+import requests
 from pyspark import StorageLevel
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import CountVectorizer
 from pyspark.sql import SparkSession
 # Setup Spark
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
+
+#set up sql defaults
 database = "baseball"
 port = "3306"
 user = "root"
 password = "x11desktop"
 
+#get table and make batter df
 bb_df = spark.read.format("jdbc").options(
     url=f"jdbc:mysql://localhost:{port}/{database}",
     driver="com.mysql.cj.jdbc.Driver",
     dbtable="batter_counts",
     user=user,
-    password=password).load()
+    password=password,inferSchema="true", header="true").load()
 
 bb_df.select("batter", "game_id", "atBat", "Hit").show()
+bb_df.createOrReplaceTempView("batter")
+bb_df.persist(StorageLevel.DISK_ONLY)
 
+#get table and make game df
 gd_df = spark.read.format("jdbc").options(
     url=f"jdbc:mysql://localhost:{port}/{database}",
     driver="com.mysql.cj.jdbc.Driver",
     dbtable="game",
     user=user,
-    password=password).load()
+    password=password,inferSchema="true", header="true").load()
 
 gd_df.select("game_id", "local_date").show()
+gd_df.createOrReplaceTempView("game")
+gd_df.persist(StorageLevel.DISK_ONLY)
+
+bat_avg = spark.sql(
+    """
+    select
+        batter
+        , atBat
+        , Hit 
+        , (Hit/nullif(atBat,0)) as game_bat_avg
+	from batter
+	"""
+    )
+bat_avg.show()
