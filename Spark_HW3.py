ab2a21d4a64d420e3c92ea2c431d2d5ccaa0c270
gramadin <33378682+gramadin@users.noreply.github.com>
2020-09-29 13:37:54 -0700

    updateed connection to make dfs
    added connectionto batter_counts and game tables. display key columns

--- Spark_HW3.py
+++ Spark_HW3.py
@@ -26,11 +26,20 @@ port = "3306"
 user = "root"
 password = "x11desktop"
 
-df = spark.read.format("jdbc").options(
+bb_df = spark.read.format("jdbc").options(
     url=f"jdbc:mysql://localhost:{port}/{database}",
     driver="com.mysql.cj.jdbc.Driver",
     dbtable="batter_counts",
     user=user,
     password=password).load()
 
-df.show()
+bb_df.select("batter", "game_id", "atBat", "Hit").show()
+
+gd_df = spark.read.format("jdbc").options(
+    url=f"jdbc:mysql://localhost:{port}/{database}",
+    driver="com.mysql.cj.jdbc.Driver",
+    dbtable="game",
+    user=user,
+    password=password).load()
+
+gd_df.select("game_id", "local_date").show()
