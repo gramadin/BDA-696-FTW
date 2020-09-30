6f9f3f17e9def960d43b288926192a90433bfd8a
gramadin <33378682+gramadin@users.noreply.github.com>
2020-09-29 18:27:46 -0700

    updated random forest and table name
    updated random forest to use hits
    changes spark table to batter_tab to remove confusion with col batter

--- HW3_spark_smythe.py
+++ HW3_spark_smythe.py
@@ -92,23 +92,16 @@ def main():
     )
 
     bb_df.select("batter", "game_id", "atBat", "Hit").show()
-    bb_df.createOrReplaceTempView("batter")
+    bb_df.createOrReplaceTempView("batter_tab")
     bb_df.persist(StorageLevel.DISK_ONLY)
 
-    # get table and make game df
-    gd_df = (
-        spark.read.format("jdbc")
-        .options(
-            url=f"jdbc:mysql://localhost:{port}/{database}",
-            driver="com.mysql.cj.jdbc.Driver",
-            dbtable="game",
-            user=user,
-            password=password,
-            inferSchema="true",
-            header="true",
-        )
-        .load()
-    )
+    #get table and make game df
+    gd_df = spark.read.format("jdbc").options(
+        url=f"jdbc:mysql://localhost:{port}/{database}",
+        driver="com.mysql.cj.jdbc.Driver",
+        dbtable="game",
+        user=user,
+        password=password,inferSchema="true", header="true").load()
 
     gd_df.select("game_id", "local_date").show()
     gd_df.createOrReplaceTempView("game")
@@ -118,23 +111,48 @@ def main():
         """
         select
             batter
+            , game_id
             , atBat
             , Hit 
             , (Hit/nullif(atBat,0)) as game_bat_avg
-        from batter
+        from batter_tab
+        order by batter, game_id
         """
-    )
+        )
     bat_avg.show()
     # make pipeline
-
+    test_df = spark.sql(
+            """
+            SELECT
+                    *
+                    , SPLIT(CONCAT(
+                        CASE WHEN batter IS NULL THEN ""
+                        ELSE batter END,
+                        " ",
+                        CASE WHEN game_id IS NULL THEN ""
+                        ELSE game_id END,
+                        " ",
+                        CASE WHEN atBat IS NULL THEN ""
+                        ELSE atBat END,
+                        " ",
+                        CASE WHEN Hit IS NULL THEN ""
+                        ELSE Hit END
+                    ), " ") AS categorical
+                FROM batter_tab
+            """
+        )
+    test_df.show()
     split_column_transform = SplitColumnTransform(
         inputCols=["batter", "game_id", "atBat", "Hit"], outputCol="categorical"
     )
+
+
+    #
     count_vectorizer = CountVectorizer(
         inputCol="categorical", outputCol="categorical_vector"
     )
     random_forest = RandomForestClassifier(
-        labelCol="batter",
+        labelCol="hit",
         featuresCol="categorical_vector",
         numTrees=100,
         predictionCol="batting_avg",
