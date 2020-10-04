bd56dc9c0663c8901976e70efb15726e64cb2b57
gramadin <33378682+gramadin@users.noreply.github.com>
2020-10-04 13:55:59 -0700

    Update HW3_spark_smythe.py

--- HW3_spark_smythe.py
+++ HW3_spark_smythe.py
@@ -26,44 +26,10 @@ from pyspark.ml.param.shared import HasInputCols, HasOutputCol
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, concat, lit, split, when
-from split_column_transform import SplitColumnTransform
 
 
-# copy from class notes for custom trasnform
-class SplitColumnTransform(
-    Transformer,
-    HasInputCols,
-    HasOutputCol,
-    DefaultParamsReadable,
-    DefaultParamsWritable,
-):
-    @keyword_only
-    def __init__(self, inputCols=None, outputCol=None):
-        super(SplitColumnTransform, self).__init__()
-        kwargs = self._input_kwargs
-        self.setParams(**kwargs)
-        return
 
-    @keyword_only
-    def setParams(self, inputCols=None, outputCol=None):
-        kwargs = self._input_kwargs
-        return self._set(**kwargs)
 
-    def _transform(self, dataset):
-        input_cols = self.getInputCols()
-        output_col = self.getOutputCol()
-
-        # Generate a list of columns to append
-        concat_list = []
-        for count, column in enumerate(input_cols):
-            column_func = when(col(column).isNull(), lit("")).otherwise(col(column))
-            concat_list.append(column_func)
-            if count < len(input_cols) - 1:
-                concat_list.append(lit(" "))
-        dataset = dataset.withColumn(
-            output_col, split(concat(*concat_list), pattern=" ")
-        )
-        return dataset
 
 
 def main():
@@ -80,7 +46,7 @@ def main():
     bb_df = (
         spark.read.format("jdbc")
         .options(
-            url=f"jdbc:mysql://localhost:{port}/{database}",
+            url=f"jdbc:mysql://localhost:{port}/{database}?zeroDateTimeBehavior=convertToNull",
             driver="com.mysql.cj.jdbc.Driver",
             dbtable="batter_counts",
             user=user,
@@ -97,7 +63,7 @@ def main():
 
     #get table and make game df
     gd_df = spark.read.format("jdbc").options(
-        url=f"jdbc:mysql://localhost:{port}/{database}",
+        url=f"jdbc:mysql://localhost:{port}/{database}?zeroDateTimeBehavior=convertToNull",
         driver="com.mysql.cj.jdbc.Driver",
         dbtable="game",
         user=user,
