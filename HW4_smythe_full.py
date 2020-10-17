23f36c0f97ab0dd09ef8624ed6d7c93d37e30fd7
gramadin <33378682+gramadin@users.noreply.github.com>
2020-10-17 12:44:45 -0700

    Not Complete
    Need to plot based on response type and predictor type
    Then fill report
    
    Currently troubleshooting exception somewhere after determining response type

--- HW4_smythe_full.py
+++ HW4_smythe_full.py
@@ -1,26 +1,22 @@
 """
 Created by Ed Smythe Oct 2020
 BDA 696 @ SDSU
-Homework - Assignment 4
-    Given a pandas dataframe
-        Contains both a response and predictors
-    Given a list of predictors and the response columns
-        Determine if response is continuous or boolean
-            (don't worry about >2 category predictions)
-        Loop through each predictor column
-            Determine if the predictor cat/cont
-            Automatically generate the necessary plot(s) to inspect it
-            Calculate the different ranking algos
-                p-value & t-score along with it's plot
-                    Regression: Continuous response
-                    Logistic Regression: Boolean response
-                Difference with mean of response along with it's plot
-                    (weighted and unweighted)
-                Random Forest Variable importance ranking
-            Generate a table with all the variables and their rankings
-    I'm going to grade this by giving it some random dataset and seeing
-        if it outputs everything
-    Desire: HTML based rankings report with links to the plots
+Given a pandas dataframe
+    Contains both a response and predictors
+Given a list of predictors and the response columns
+    Determine if response is continuous or boolean (don't worry about >2 category responses)
+    Loop through each predictor column
+        Determine if the predictor cat/cont
+        Automatically generate the necessary plot(s) to inspect it
+        Calculate the different ranking algos
+        p-value & t-score along with it's plot
+        Regression: Continuous response
+        Logistic Regression: Boolean response
+        Difference with mean of response along with it's plot (weighted and unweighted)
+        Random Forest Variable importance ranking
+        Generate a table with all the variables and their rankings
+I'm going to grade this by giving it some random dataset and seeing if it outputs everything
+Desire: html or excel based rankings report with links to the plots
 
     Recommendations for plots
     Predictor / Response type Dependant
@@ -37,23 +33,34 @@ Homework - Assignment 4
             Predictor: Continuous
                 Scatter plot with trendline
 """
-import sys
-import webbrowser
-from io import StringIO
 
+import sys
+
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import pydot
 import statsmodels.api as sm
+import webbrowser
+
+from datetime import datetime
+from io import StringIO
 from pandas import DataFrame
+from plotly import express as px
+from plotly import figure_factory as ff
 from plotly import graph_objects as go
 from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.utils.multiclass import type_of_target as tot
 
 
-# Ploting Continuous data
-def plot_continuous(to_plot):
+# PLOT Functions
+def plot_cont_cont(to_plot):
+    # Scatter plot with trendline
+    # from class notes Week6
     user_db = to_plot
     X = user_db.data
     y = user_db.target
@@ -81,32 +88,115 @@ def plot_continuous(to_plot):
     return
 
 
-def plot_bool(to_plot):
+def plot_cont_cat(to_plot):
+    # Violin plot on predictor grouped by response 
     user_db = to_plot
     X = user_db.data
     y = user_db.target
+    #
     for idx, column in enumerate(X.T):
         feature_name = user_db.feature_names[idx]
         predictor = sm.add_constant(column)
-        log_regression_model = sm.Logit(y, predictor)
-        log_regression_model_fitted = log_regression_model.fit()
+        #
+        logistic_regression_model = LogisticRegression(
+            random_state=4622).fit(predictor, y)
         print(f"Variable: {feature_name}")
-        print(log_regression_model_fitted.summary())
-
-        # Get the stats
-        t_value = round(log_regression_model_fitted.tvalues[1], 6)
-        p_value = "{:.6e}".format(log_regression_model_fitted.pvalues[1])
-
+        print(logistic_regression_model.score(predictor, y))
+        # Get the score
+        the_score = logistic_regression_model.score(predictor, y)
         # Plot the figure
-        fig = px.scatter(x=column, y=y, trendline="ols")
+        fig = px.violin(x=y, y=column, color=y)
         fig.update_layout(
-            title=f"Variable: {feature_name}: (t-value={t_value}) (p-value={p_value})",
+            title=f"Variable:{feature_name},Logistic Regression Score={the_score}",
             xaxis_title=f"Variable: {feature_name}",
             yaxis_title="y",
         )
         fig.show()
-        fig.write_html(file=f"./HW4_log_plot{idx}.html", include_plotlyjs="cdn")
+        fig.write_html(file=f"./HW4_cont_cat_violin_plot.html", include_plotlyjs="cdn")
     return
+
+
+def plot_cat_cat(to_plot):
+    # haven't figured out how to implement a z in a Heatplot here, using violin
+    user_db = to_plot
+    X = user_db.data
+    y = user_db.target
+    #
+    for idx, column in enumerate(X.T):
+        feature_name = user_db.feature_names[idx]
+        predictor = sm.add_constant(column)
+        #
+        logistic_regression_model = LogisticRegression(
+            random_state=4622).fit(predictor, y)
+        print(f"Variable: {feature_name}")
+        print(logistic_regression_model.score(predictor, y))
+        # Get the score
+        the_score = logistic_regression_model.score(predictor, y)
+        # Plot the Figure
+        fig = px.violin(x=y, y=column)
+        fig.update_layout(
+            title=f"Variable:{feature_name},Logistic Regression Fit Score={the_score}",
+            xaxis_title=f"Variable: {feature_name}",
+            yaxis_title="y",
+        )
+        fig.show()
+        fig.write_html(file=f"./HW4_cat_cat_violin_plot.html", include_plotlyjs="cdn")
+    return
+
+
+def plot_cat_cont(to_plot):
+    # Violin plot on response grouped by predictor
+    user_db = to_plot
+    X = user_db.data
+    y = user_db.target
+    #
+    for idx, column in enumerate(X.T):
+        feature_name = user_db.feature_names[idx]
+        predictor = sm.add_constant(column)
+        #
+        logistic_regression_model = LogisticRegression(
+            random_state=4622).fit(predictor, y)
+        print(f"Variable: {feature_name}")
+        print(logistic_regression_model.score(predictor, y))
+        # Get the score
+        the_score = logistic_regression_model.score(predictor, y)
+        # Plot the Figure
+        fig = px.violin(x=column, y=y, color=column)
+        fig.update_layout(
+            title=f"Variable:{feature_name},Logistic Regression Fit Score={the_score}",
+            xaxis_title=f"Variable: {feature_name}",
+            yaxis_title="y",
+        )
+        fig.show()
+        fig.write_html(file=f"./HW4_cat_con_violin_plot{idx}.html",
+                       include_plotlyjs="cdn")
+    return
+
+
+# add boolean check incase type check is wrong
+def col_is_bool(df, col):
+    c = len(df[col].unique())
+    if c == 2:
+        True
+    else:
+        False
+    return col
+
+
+# predictor select for any dataset
+def predictor_select(ds):
+    predictors = ds.feature_names
+    # index_list = [i+1 for i in range(len(predictors))]
+    selection_df = pd.DataFrame(predictors, columns=["Predictor"])
+    selection_df.index += 1
+    print(selection_df)
+    pred_choice = input("Which predictors will you use? (use index numbers separate by a comma) :")
+    pred_choice = pred_choice.split(",")
+    for i in range(0, len(pred_choice)):
+        pred_choice[i] = int(pred_choice[i])
+    feature_list = [selection_df.at[i, "Predictor"] for i in pred_choice]
+
+    return feature_list
 
 
 def print_heading(title):
@@ -116,37 +206,8 @@ def print_heading(title):
     return
 
 
-def plot_decision_tree(decision_tree, feature_names, class_names, file_out):
-    with StringIO() as dot_data:
-        export_graphviz(
-            decision_tree,
-            feature_names=feature_names,
-            class_names=class_names,
-            out_file=dot_data,
-            filled=True,
-        )
-        graph = pydot.graph_from_dot_data(dot_data.getvalue())
-        graph[0].write_pdf(file_out + ".pdf")  # must access graph's first element
-        graph[0].write_png(file_out + ".png")  # must access graph's first element
-
-
-"""
-def col_is_bool(df, col):  # col not working when placed into 'sepel_width_cm_'
-    global bool_test
-    titles = [i.replace(")", "_").replace("(", "").replace(" ", "_") for i in col_list]
-    df.columns = titles
-    df.sepel_width_cm_.unique()
-    c = len(working_df.sepel_width_cm_.unique())
-    if c == 2:
-        bool_test = True
-    else:
-        bool_test = False
-    return
-"""
-
-
+# Main functions
 def main():
-
     #    start_t = datetime.now()
     # set built-in dataset choices
     ds1 = datasets.load_boston()
@@ -158,15 +219,17 @@ def main():
     ds7 = datasets.load_breast_cancer()
 
     # Get user inputs -- add error handeling
+    # when response is continous then no 'target_names'
+    # when response is catagorical set 'target_names'
     print(
         """ Select SKLearn dataset to use: ~default is 3~
-                    1 - boston
-                    2 - iris
-                    3 - diabetes
-                    4 - digits
-                    5 - linnerud
-                    6 - wine
-                    7 - breast cancer
+                    1 - boston (response is continuous)
+                    2 - iris (response is catagorical [3 cats])
+                    3 - diabetes (response is continuous)
+                    4 - digits (response is an array) Not working with this version
+                    5 - linnerud (response is catagorical array of continuous)  [3 cats]) Not working with this version
+                    6 - wine (response is catagorical [3 cast])
+                    7 - breast cancer (response is catagorical {2cats/bool])
                     8 - user choice
                   """
     )
@@ -195,142 +258,61 @@ def main():
         print(f"{choice} is an invalid entry. Please try again.")
 
     print(f"You chose {choice}")
-
-    # turn into a pandas df
+    # turn source data into a pandas df
     working_df = pd.DataFrame(the_ds.data, columns=the_ds.feature_names)
-    working_df["target"] = pd.Series(the_ds.target)
+    working_df["target"] = pd.Series(the_ds.target)  # chg
     working_df.head()
 
     # get column names
-    # col_list = working_df.columns.values.tolist()
-    # titles =
-    # [i.replace(")", "_").replace("(", "").replace(" ", "_") for i in col_list]
-    # working_df.columns = titles
 
-    # Check if boolean
-    # bool_list = [col_is_bool(working_df,i) for i in titles]
-    # use when functions is working
-
-    # Increase pandas print viewport (so we see more on the screen)
-    pd.set_option("display.max_rows", 60)
-    pd.set_option("display.max_columns", 500)
-    pd.set_option("display.width", 1_040)
-
-    # working_df.columns = col_list
-
-    # Drop rows with missing values
-    # working_df = working_df.dropna()
+    col_list = working_df.columns.values.tolist()
+    titles = [i.replace(')', "_").replace('(', "").replace(' ', '_') for i in col_list]
+    working_df.columns = titles
 
     print_heading("Original Dataset")
     print(working_df)
 
-    # Continuous Features ~ use a function to get which is continus
-    # Need to find out why putting col_list[:-1] does not work here
-    continuous_features = [
-        "sepal length (cm)",
-        "sepal width (cm)",
-        "petal length (cm)",
-        "petal width (cm)",
-    ]  # chg
-    X = working_df[continuous_features].values
+    # ~RESPONSE~
+    y = the_ds.target  # assign the response here #chg
+    res_type = tot(the_ds.target)
+    print_heading("Response type is " + res_type)
 
-    # Response
-    y = working_df["target"].values  # chg
+    # Group Predictor types
+    type_mask = [tot(working_df[i]) for i in col_list]
+    predictor_array = np.column_stack((col_list, type_mask))
+    pred_df = pd.DataFrame(predictor_array, columns=["Predictor", "Type"])
 
-    # Decision Tree Classifier
-    max_tree_depth = 7
-    tree_random_state = 0  # Always set a seed
-    decision_tree = DecisionTreeClassifier(
-        max_depth=max_tree_depth, random_state=tree_random_state
+    # ~PREDICTORS~
+    # Allow user to select the desired features
+    # feature_list = predictor_select(the_ds) #gettgin an exception with some DS
+
+    cont_feature_df = pred_df[pred_df['Type'] == 'continuous']  # where tot continuous and bool_check false
+    cat_feature_df = pred_df[pred_df['Type'] != 'continuous']  # where tot not contious or bool_check true
+    if cat_feature_df.empty:
+        print('No Catagorical!')
+    if cont_feature_df.empty:
+        print('No Continuous!')
+    cont_feature_list = list(cont_feature_df['Predictor'])
+    print('Continuous predictors ' + cont_feature_list)
+    cat_feature_list = list(cat_feature_df['Predictor'])
+    print('Catagorical predictors ' +cat_feature_list)
+
+    # Generate Report DF
+    report_time = datetime.now().isoformat()
+    report_col = (
+        "Catagory",
+        "p-val_&_t-val",
+        "Regression",
+        "Logistic_Regression",
+        "Difference_with_mean",
+        "Random_Forest",
     )
-    decision_tree.fit(X, y)
+    # report_df_cont = pd.DataFrame("", index=continuous_features, columns=report_col)
+    # report_df_cat = pd.DataFrame("", index=catagorical_features, columns=report_col)
 
-    # Plot the decision tree
-    plot_decision_tree(
-        decision_tree=decision_tree,
-        feature_names=continuous_features,
-        class_names="classification",
-        file_out="./hw4_tree",
-    )
-
-    # Find an optimal tree via cross-validation
-    parameters = {
-        "max_depth": range(1, max_tree_depth),
-        "criterion": ["gini", "entropy"],
-    }
-    decision_tree_grid_search = GridSearchCV(
-        DecisionTreeClassifier(random_state=tree_random_state), parameters, n_jobs=4
-    )
-    decision_tree_grid_search.fit(X=X, y=y)
-
-    cv_results = DataFrame(decision_tree_grid_search.cv_results_["params"])
-    cv_results["score"] = decision_tree_grid_search.cv_results_["mean_test_score"]
-    print_heading("Cross validation results")
-    print(cv_results)
-    print_heading("Cross validation results - HTML table")
-    print(cv_results.to_html())
-
-    # Plot these cross_val results
-    gini_results = cv_results.loc[cv_results["criterion"] == "gini"]
-    entropy_results = cv_results.loc[cv_results["criterion"] == "entropy"]
-    data = [
-        go.Scatter(
-            x=gini_results["max_depth"].values,
-            y=gini_results["score"].values,
-            name="gini",
-            mode="lines",
-        ),
-        go.Scatter(
-            x=entropy_results["max_depth"].values,
-            y=entropy_results["score"].values,
-            name="entropy",
-            mode="lines",
-        ),
-    ]
-
-    layout = go.Layout(
-        title="Cross Validation",
-        xaxis_title="Tree Depth",
-        yaxis_title="Score",
-    )
-
-    fig = go.Figure(data=data, layout=layout)
-    fig.show()
-    fig.write_html(
-        file="HW4_cross_val.html",
-        include_plotlyjs="cdn",
-    )
-
-    # Get the "best" model
-    best_tree_model = decision_tree_grid_search.best_estimator_
-
-    # Plot this "best" decision tree
-    plot_decision_tree(
-        decision_tree=best_tree_model,
-        feature_names=continuous_features,
-        class_names="classification",
-        file_out="HW4_cross_val",
-    )
-    #    F" {(datetime.now() - start_t)} seconds"
-    file = open("report.html", "wb")
-
-    # not working
-    code = """<html>
-    <head>esmythe report</head>
-    <body><p>report</p>
-
-        <img src="HW4_tree.png" alt="cross_val">
-
-    </body>
-
-    </html>"""
-    file.write(code)
-    file.close()
-
-    webbrowser.open("report.html")
-
-    return
-
+    # Save report to HTML
+    report_df = working_df
+    report_df.to_html("HW_report_" + datetime.now().strftime("%Y_%m_%d") + ".html")
 
 if __name__ == "__main__":
     sys.exit(main())
