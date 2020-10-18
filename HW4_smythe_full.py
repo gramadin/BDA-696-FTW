2e84594b00bcf87cc04894678f6117f7ae273b7c
gramadin <33378682+gramadin@users.noreply.github.com>
2020-10-17 20:27:46 -0700

    pre commits
    repaired with pre commit linters

--- HW4_smythe_full.py
+++ HW4_smythe_full.py
@@ -1,11 +1,11 @@
 """
 Created by Ed Smythe Oct 2020
 BDA 696 @ SDSU
-Current run time ~ 7 seconds
 Given a pandas dataframe
     Contains both a response and predictors
 Given a list of predictors and the response columns
-    Determine if response is continuous or boolean (don't worry about >2 category responses)
+    Determine if response is continuous or boolean
+    (don't worry about >2 category responses)
     Loop through each predictor column
         Determine if the predictor cat/cont
         Automatically generate the necessary plot(s) to inspect it
@@ -16,7 +16,8 @@ Given a list of predictors and the response columns
         Difference with mean of response along with it's plot (weighted and unweighted)
         Random Forest Variable importance ranking
         Generate a table with all the variables and their rankings
-I'm going to grade this by giving it some random dataset and seeing if it outputs everything
+I'm going to grade this by giving it some random dataset
+and seeing if it outputs everything
 Desire: html or excel based rankings report with links to the plots
 
     Recommendations for plots
@@ -36,25 +37,14 @@ Desire: html or excel based rankings report with links to the plots
 """
 
 import sys
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import pydot
 import statsmodels.api as sm
-import webbrowser
-
-from datetime import datetime
-from io import StringIO
-from pandas import DataFrame
-from plotly import express as px
-from plotly import figure_factory as ff
-from plotly import graph_objects as go
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.utils.multiclass import type_of_target as tot
 
 
@@ -98,8 +88,9 @@ def plot_cont_cat(to_plot):
         feature_name = user_db.feature_names[idx]
         predictor = sm.add_constant(column)
         #
-        logistic_regression_model = LogisticRegression(
-            random_state=4622).fit(predictor, y)
+        logistic_regression_model = LogisticRegression(random_state=4622).fit(
+            predictor, y
+        )
         print(f"Variable: {feature_name}")
         print(logistic_regression_model.score(predictor, y))
         # Get the score
@@ -125,8 +116,9 @@ def plot_cat_cat(to_plot):
         feature_name = user_db.feature_names[idx]
         predictor = sm.add_constant(column)
         #
-        logistic_regression_model = LogisticRegression(
-            random_state=4622).fit(predictor, y)
+        logistic_regression_model = LogisticRegression(random_state=4622).fit(
+            predictor, y
+        )
         print(f"Variable: {feature_name}")
         print(logistic_regression_model.score(predictor, y))
         # Get the score
@@ -152,8 +144,9 @@ def plot_cat_cont(to_plot):
         feature_name = user_db.feature_names[idx]
         predictor = sm.add_constant(column)
         #
-        logistic_regression_model = LogisticRegression(
-            random_state=4622).fit(predictor, y)
+        logistic_regression_model = LogisticRegression(random_state=4622).fit(
+            predictor, y
+        )
         print(f"Variable: {feature_name}")
         print(logistic_regression_model.score(predictor, y))
         # Get the score
@@ -165,8 +158,9 @@ def plot_cat_cont(to_plot):
             xaxis_title=f"Variable: {feature_name}",
             yaxis_title="y",
         )
-        fig.write_html(file=f"./HW4_cat_con_violin_plot{idx}.html",
-                       include_plotlyjs="cdn")
+        fig.write_html(
+            file=f"./HW4_cat_con_violin_plot{idx}.html", include_plotlyjs="cdn"
+        )
     return
 
 
@@ -187,7 +181,9 @@ def predictor_select(ds):
     selection_df = pd.DataFrame(predictors, columns=["Predictor"])
     selection_df.index += 1
     print(selection_df)
-    pred_choice = input("Which predictors will you use? (use index numbers separate by a comma) :")
+    pred_choice = input(
+        "Which predictors will you use? (use index numbers separate by a comma) :"
+    )
     pred_choice = pred_choice.split(",")
     for i in range(0, len(pred_choice)):
         pred_choice[i] = int(pred_choice[i])
@@ -202,14 +198,15 @@ def print_heading(title):
     print("*" * 80)
     return
 
-def list_to_df(from_df,to_df,col):
+
+def list_to_df(from_df, to_df, col):
     temp_list = from_df[col].to_list()
     to_df[col] = pd.Series(temp_list, index=to_df.index)
 
 
 # Main functions
 def main():
-    #    start_t = datetime.now()
+    start_t = datetime.now()
     # set built-in dataset choices
     ds1 = datasets.load_boston()
     ds2 = datasets.load_iris()
@@ -227,8 +224,8 @@ def main():
                     1 - boston (response is continuous)
                     2 - iris (response is catagorical [3 cats])
                     3 - diabetes (response is continuous)
-                    4 - digits (response is an array) Not working with this version
-                    5 - linnerud (response is catagorical array of continuous)  [3 cats]) Not working with this version
+                    4 - digits (response is an array)
+                    5 - linnerud (response is catagorical array of continuous)[3 cats])
                     6 - wine (response is catagorical [3 cast])
                     7 - breast cancer (response is catagorical {2cats/bool])
                     8 - user choice
@@ -267,13 +264,13 @@ def main():
     # get column names
 
     col_list = working_df.columns.values.tolist()
-    titles = [i.replace(')', "_").replace('(', "").replace(' ', '_') for i in col_list]
+    titles = [i.replace(")", "_").replace("(", "").replace(" ", "_") for i in col_list]
     working_df.columns = titles
     print_heading("Original Dataset")
     print(working_df)
 
     # ~RESPONSE~
-    y = the_ds.target  # assign the response here #chg
+    # y = the_ds.target  # assign the response here #chg
     res_type = tot(the_ds.target)
     print_heading("Response type is " + res_type)
 
@@ -284,25 +281,34 @@ def main():
 
     # ~PREDICTORS~
     # Allow user to select the desired features
-    feature_list = predictor_select(the_ds)  # gettgin an exception with some DS
+    # feature_list = predictor_select(the_ds)
+    # enable abovce line to select only specific features to evaluate
 
-    cont_feature_df = pred_df[pred_df['Category'] == 'continuous']  # where tot continuous and bool_check false
+    cont_feature_df = pred_df[
+        pred_df["Category"] == "continuous"
+    ]  # where tot continuous and bool_check false
     try:
-        cont_feature_df = cont_feature_df = cont_feature_df.drop('target', axis=1, inplace=True)
+        cont_feature_df = cont_feature_df = cont_feature_df.drop(
+            "target", axis=1, inplace=True
+        )
     except Exception:
         pass
 
-    cat_feature_df = pred_df[pred_df['Category'] != 'continuous']  # where tot not continuous or bool_check true
+    cat_feature_df = pred_df[
+        pred_df["Category"] != "continuous"
+    ]  # where tot not continuous or bool_check true
     try:
-        cat_feature_df = cat_feature_df = cat_feature_df.drop('target', axis=1, inplace=True)
+        cat_feature_df = cat_feature_df = cat_feature_df.drop(
+            "target", axis=1, inplace=True
+        )
     except Exception:
         pass
 
-    print('No Continuous!') if cont_feature_df.empty else print(cont_feature_df)
-    print('No Categorical!') if cat_feature_df.empty else print(cat_feature_df)
+    print("No Continuous!") if cont_feature_df.empty else print(cont_feature_df)
+    print("No Categorical!") if cat_feature_df.empty else print(cat_feature_df)
 
-    cont_feature_list = list(cont_feature_df['Predictor'])
-    cat_feature_list = list(cat_feature_df['Predictor'])
+    # cont_feature_list = list(cont_feature_df["Predictor"])
+    # cat_feature_list = list(cat_feature_df["Predictor"])
 
     # Make plots
     if res_type == "continuous":
@@ -328,13 +334,17 @@ def main():
     # Update Report with data
     report_df_cont.index.name = "Predictor"
     pred_df.index = report_df_cont.index
-    pred_df = pred_df.set_index(['Predictor'])
-    list_to_df(pred_df, report_df_cont, 'Category')
+    pred_df = pred_df.set_index(["Predictor"])
+    list_to_df(pred_df, report_df_cont, "Category")
 
     # Save report to HTML
     report_df = report_df_cont
-    report_df.to_html("HW_report_" + datetime.now().strftime("%Y_%m_%d") + ".html")
-    print('check')
+    report_df.to_html(
+        "HW_report_" + datetime.now().strftime("%Y_%m_%d") + {report_time} + ".html"
+    )
+    print(datetime.now() - start_t)
     return
+
+
 if __name__ == "__main__":
     sys.exit(main())
