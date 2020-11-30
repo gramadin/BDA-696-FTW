7f3a1010dda650913fc1bb19ea4be52fd7f4d4bc
Julien Pierret <dafrenchyman@gmail.com>
2020-11-29 18:32:32 -0800

    Making this work a little more conventionally

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,13 @@
-# BASE IMAGE
-FROM mariadb:10.5.8
+# base image
+FROM ubuntu
 
-# Me
-MAINTAINER gramadin@databygram.com
+# Install mysql client
+RUN apt-get update
+RUN apt-get install -y mysql-client
 
-# create and use folder for the image
+# copy over code
+COPY baseball.sql /data/baseball.sql
+COPY sql /scripts
 
-WORKDIR .
-
-
-# get requirements
-COPY requirements.txt .
-COPY baseball.sql .
-
-#copy all that to the working directory
-#COPY app.py ./app.py
-
-# get and unpack the SQL file
-#RUN curl https://teaching.mrsharky.com/data/baseball.sql.tar.gz \
-#  | tar -xjC /tmp/package \
-#  && make -C /tmp/package
-
-# install reqs
-# RUN pip install --no-cache-dir -r requirements.txt
-
-RUN echo Please wait....
-RUN docker container exec -i db-container mysql bbdb < baseball.sql -ppass
-Run echo Complete
-
-
-# use docker build to build image - 'docker build --tag test1 .' dot is local directory
-# copy things that dont change often first. this uses cache most efficently
-
+# command to run on container start
+CMD ["/scripts/code.sh"]
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker-compose up -d
 echo Please wait... Loading bbdb
 sleep 10s
@@ -5,7 +6,7 @@ docker container exec -i db-container mysql bbdb < baseball.sql -ppass
 echo Complete
 docker exec -i db-container mysql bbdb -u root -ppass -e "drop table if exists rolling_100;"
 echo Making rolling table
-docker exec -i db-container mysql bbdb -u root -ppass -e "create table rolling_100 as select g.game_id, g.local_date, bc.batter, sum(bc.atBat) as atbats, sum(bc.Hit) as Hits, count(*) as chk from batter_counts bc join game g on g.game_id = bc.game_id join batter_counts bc1 on bc.batter = bc1.batter join game g1 on g1.game_id = bc1.game_id and g.local_date >= date_add(g.local_date, interval -100 day) where g.local_date < '2011-04-04 15:05:00' and bc.batter in (select batter from batter_counts bc2 where game_id = '12560') group by batter, g.game_id order by batter;"
+docker exec -i db-container mysql bbdb -u root -ppass -e ""
 echo exporting
 docker exec -it db-container bash -c 'mysql -h localhost -u root -ppass --database bbdb --batch -e "select batter,(sum(Hits)/sum(atbats)) as batavg from rolling_100 group by batter order by batter;"'  > report.txt
 sed -e 's/\s\+/,/g' report.txt > report.csv
